9199d1d2c14b7293a3493433bba18d416deb72da
Tom Jacobs <tjacobs@expensify.com>
2017-05-28 18:16:33 +1000

    Cartpole mutation

--- agents/cartpole.py
+++ agents/cartpole.py
@@ -13,10 +13,16 @@
 # Method to use?
 method = 2
 
+# Tunable params for method 2
+episodes_per_update = 20 # Try each mutation with 20 episodes
+mutation_amount = 1.0    # 100% random to start with
+mutation_decay = 0.01    # How much we reduce mutation_amount by, each run
+
 # Submit it?
 submit = True
 api_key = ''
 
+# Imports
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
@@ -40,11 +46,13 @@ def run_episode(env, parameters):
 
         # Done?
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            #print("Episode finished after {} timesteps".format(t+1))
             break
     return total_reward
 
 def train(submit):
+    global mutation_amount
+
     # Start cartpole
     env = gym.make('CartPole-v0')
     if submit:
@@ -52,20 +60,16 @@ def train(submit):
 
     # Keep results
     results = []
-    counter = 0
 
-    # For method 1. Run lots of episodes with random params, and find the best_parameters.
+    # For method 1 and 2. Run lots of episodes with random params, and find the best_parameters.
     best_parameters = None
     best_reward = 0
 
-    # Additional for method 2
-    episodes_per_update = 5
-    mutation_amount = 0.5
+    # Additional for method 2, start off with random parameters
     best_parameters = np.random.rand(4) * 2 - 1
 
     # Run
     for t in range(100):
-        counter += 1
 
         # Pick random parameters and run
         if method == 1:
@@ -75,6 +79,7 @@ def train(submit):
         # Method 2 is to use the best parameters, with 10% random mutation
         elif method == 2:
             new_parameters = best_parameters + (np.random.rand(4) * 2 - 1) * mutation_amount
+            mutation_amount = max(0, mutation_amount - mutation_decay)
             reward = 0
             for e in range(episodes_per_update):
                  run = run_episode(env, new_parameters)
@@ -92,7 +97,7 @@ def train(submit):
 
             # And did we win the world?
             if reward == 200:
-                print("Win! Episode {}".format(t))
+                print("Win! Episode {}".format(t+1))
                 break # Can't do better than 200 reward, so quit trying
 
     # Run 100 runs with the best found params
@@ -100,25 +105,29 @@ def train(submit):
     for t in range(100):
         reward = run_episode(env, best_parameters)
         results.append(reward)
-        print( "Episode " + str(t) )
+#        print( "Episode " + str(t) )
 
     # Done
     return results
 
-# Run
-results = train(submit=submit)
-if submit:
-    # Submit to OpenAI Gym
-    print("Uploading to gym")
-    gym.scoreboard.api_key = api_key
-    gym.upload('cartpole')
+# Try 10 times for a quick learn
+for t in range(10):
+    results = train(submit=submit)
+    if submit:
+        # Submit to OpenAI Gym if learned quickly enough
+        print("Number of episodes run: {}".format(len(results)))
+        if len(results) < 120:
+            print("Uploading to gym...")
+            gym.scoreboard.api_key = api_key
+            gym.upload('cartpole')
+            break
 
-else:
-    # Graph
-    plt.plot(results)
-    plt.xlabel('Episode')
-    plt.ylabel('Rewards')
-    plt.title('Rewards over time')
-    plt.show()
+    else:
+        # Graph
+        plt.plot(results)
+        plt.xlabel('Episode')
+        plt.ylabel('Rewards')
+        plt.title('Rewards over time')
+        plt.show()
 
 
